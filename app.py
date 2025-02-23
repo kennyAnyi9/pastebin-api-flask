86e87c1928afe9d8ad8e0e341e424d42c49d95c5
Kennedy Anyidoho <120096792+kennyAnyi9@users.noreply.github.com>
2025-02-23 16:05:35 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,14 @@
-# app.py
 from flask import Flask, request, jsonify
 from database import create_paste, get_paste
 from flask_cors import CORS
-
-
 
 app = Flask(__name__)
 
 allowed_origins = [
     "http://localhost:3000",
-    "https://pasteit-mauve.vercel.app/",
-    "https://www.pasteit-mauve.vercel.app/",
     "https://pasteit-mauve.vercel.app",
     "https://www.pasteit-mauve.vercel.app"
 ]
-
-
 CORS(app, resources={r"/api/*": {"origins": allowed_origins}})
 
 @app.route('/api/paste', methods=['POST'])
@@ -24,7 +17,7 @@ def create_paste_route():
     if not content:
         return jsonify({'error': 'No content provided'}), 400
     paste_id = create_paste(content)
-    url = f"https://pastebin-7j39.onrender.com/api/paste/{paste_id}"  # Update this for production
+    url = f"https://pastebin-7j39.onrender.com/api/paste/{paste_id}"
     return jsonify({'url': url}), 201
 
 @app.route('/api/paste/<paste_id>', methods=['GET'])
@@ -35,4 +28,4 @@ def get_paste_content(paste_id):
     return jsonify({'error': 'Paste not found'}), 404
 
 if __name__ == '__main__':
-    app.run()  
+    app.run()
